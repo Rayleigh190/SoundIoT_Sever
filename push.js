d26a97bc97b48dc06b8802aef3777194ef96fcdd
rayleigh <rayleigh190@gmail.com>
2022-11-18 21:46:00 +0900

    change android token

--- push.js
+++ push.js
@@ -6,8 +6,12 @@ adminAndroid.initializeApp({
   credential: adminAndroid.credential.cert(androidServiceAccount),
 });
 
-var androidRegistrationToken =
-  "d_RbtI6TT_yZZdTTqQnohc:APA91bEAl-JJNa4fq3rTD3ZsxRp14R5Rr2tFr2aXokO656F1DeUIKfwfTFMYTcxZvjpWCkiojQjJTuVxe7IqihhqyIo0ScDzNpZSO5Z12n_LpY-o47rVBg_kwwHRLTkzvnGzmD6j40jE";
+// var androidRegistrationToken =
+//   "d_RbtI6TT_yZZdTTqQnohc:APA91bEAl-JJNa4fq3rTD3ZsxRp14R5Rr2tFr2aXokO656F1DeUIKfwfTFMYTcxZvjpWCkiojQjJTuVxe7IqihhqyIo0ScDzNpZSO5Z12n_LpY-o47rVBg_kwwHRLTkzvnGzmD6j40jE";
+var fcmTokens = [
+  "eIu61M2_T5a5UBUh_0Rszf:APA91bHMnL5sY94141en0oC-kKrnpjxjAj5XR4C8ED7mWGwrvpXSfYbPa3lsfBqE8JCUzfAQSkltUsd7WHMfywH3RchRqTK5jD31dR8TH4Ch7Jto3lfne1TKB0xPNGIVheJDJYNJ_Das",
+  "d_RbtI6TT_yZZdTTqQnohc:APA91bEAl-JJNa4fq3rTD3ZsxRp14R5Rr2tFr2aXokO656F1DeUIKfwfTFMYTcxZvjpWCkiojQjJTuVxe7IqihhqyIo0ScDzNpZSO5Z12n_LpY-o47rVBg_kwwHRLTkzvnGzmD6j40jE",
+];
 
 let soundStr = ["살려주세요", "도와주세요", "울음소리"];
 
@@ -19,7 +23,7 @@ module.exports = {
           "김민영님의 '" + soundStr[soundID - 1] + "' 소리가 인식 됐습니다.",
         body: "김민영님의 위치를 확인해 보세요!",
       },
-      token: androidRegistrationToken,
+      tokens: fcmTokens,
     };
 
     console.log("message push");
